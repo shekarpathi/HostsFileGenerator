40a5f3d3e378f74c66a7c0cc91a8663b3ffb16db
shekarpathi <tambaram@gmail.com>
2022-07-14 12:20:41 -0400

    Update foo.py

--- python/foo.py
+++ python/foo.py
@@ -5,6 +5,8 @@ import os
 # initialize exception domains, these will be allowed
 allowable_domains_set = ["fe80::1%lo0 localhost", ".godaddy.com", "sso.godaddy.com", ".etihad.com", "www.googleadservices.com", "ad.doubleclick.net", "go.redirectingat.com", "www.jdoqocy.com", "cj.dotomi.com", "go.redirectingat.com"]
 
+block_url = True
+
 url = 'https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts'
 r = requests.get(url, allow_redirects=True)
 open('hosts.txt', 'wb').write(r.content)
@@ -38,18 +40,22 @@ output_file = open(output_file_path, 'w')
 
 #4
 for line in open('hosts.txt', 'r'):
+  block_url = True
   # if (line.rstrip() == "fe80::1%lo0 localhost"):
   #   continue
   for x in allowable_domains_set:
    if (x in line.rstrip()):
-     continue
+     print(x)
+     block_url = False
+     break
       
   #5
   hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
   #6
   if hashValue not in completed_lines_hash:
-    output_file.write(line)
-    completed_lines_hash.add(hashValue)
+    if block_url == True:
+      output_file.write(line)
+      completed_lines_hash.add(hashValue)
 #7
 output_file.close()
 if os.path.exists("hosts.txt"):
