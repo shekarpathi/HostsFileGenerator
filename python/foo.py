73fb039d45e0bae786e804cab8d1de01ca24af84
shekarpathi <tambaram@gmail.com>
2022-07-14 08:28:11 -0400

    Update foo.py

--- python/foo.py
+++ python/foo.py
@@ -3,15 +3,15 @@ import hashlib
 import os
 
 # initialize exception domains, these will be allowed
-# allowable_domains_set = [".godaddy.com", "sso.godaddy.com", ".etihad.com", "www.googleadservices.com", "ad.doubleclick.net", "go.redirectingat.com", "www.jdoqocy.com", "cj.dotomi.com", "go.redirectingat.com"]
+allowable_domains_set = ["fe80::1%lo0 localhost", ".godaddy.com", "sso.godaddy.com", ".etihad.com", "www.googleadservices.com", "ad.doubleclick.net", "go.redirectingat.com", "www.jdoqocy.com", "cj.dotomi.com", "go.redirectingat.com"]
 
 url = 'https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts'
 r = requests.get(url, allow_redirects=True)
 open('hosts.txt', 'wb').write(r.content)
 
-# url = 'https://hosts.ubuntu101.co.za/hosts'
-# r = requests.get(url, allow_redirects=True)
-# open('hosts.txt', 'ab').write(r.content)
+url = 'https://hosts.ubuntu101.co.za/hosts'
+r = requests.get(url, allow_redirects=True)
+open('hosts.txt', 'ab').write(r.content)
 
 output_file_path = 'hosts.add'
 #2
@@ -22,11 +22,11 @@ output_file = open(output_file_path, 'w')
 
 #4
 for line in open('hosts.txt', 'r'):
-  if (line.rstrip() == "fe80::1%lo0 localhost"):
-    continue
-  # for x in allowable_domains_set:
-  #  if (x in line.rstrip()):
-  #    continue
+  # if (line.rstrip() == "fe80::1%lo0 localhost"):
+  #   continue
+  for x in allowable_domains_set:
+   if (x in line.rstrip()):
+     continue
       
   #5
   hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
